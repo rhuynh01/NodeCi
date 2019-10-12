63142a28caf1bec26bffc717970db1511bd50982
Rodney Huynh <rhuynh2005@gmail.com>
2019-10-12 09:21:33 -0500

    removed commented test cases

---
+++ playground/dummytestcase.js
@@ -0,0 +1,18 @@
+// inside header.test.js file
+// test('should click the link Login With Google and start oauth flow ', async () => {
+//   await page.click('.right a');
+
+//   const url = await page.url();
+//   expect(url).toMatch(/accounts\.google\.com/);
+// });
+
+// test('should show logout button when user is signed in', async () => {
+//   await page.login();
+
+//   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+//   expect(text).toEqual('Logout');
+// });
+
+// -------------------------------------------------------
+//
--- tests/header.test.js
+++ tests/header.test.js
@@ -15,19 +15,4 @@ describe('Header testing', () => {
     const text = await page.getContentsOf('a.brand-logo');
     expect(text).toEqual('Blogster');
   });
-
-  // test('should click the link Login With Google and start oauth flow ', async () => {
-  //   await page.click('.right a');
-
-  //   const url = await page.url();
-  //   expect(url).toMatch(/accounts\.google\.com/);
-  // });
-
-  // test.skip('should show logout button when user is signed in', async () => {
-  //   await page.login();
-
-  //   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
-
-  //   expect(text).toEqual('Logout');
-  // });
 });
